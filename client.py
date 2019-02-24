55c0909cdba46afdd2a2ba6d2ac8829c4438fbd6
CWJWANJING <wanjingchencwj@gmail.com>
2019-02-24 22:41:10 +0000

    Rearranged print statements as requested,should still work,will test it

--- client.py
+++ client.py
@@ -15,30 +15,37 @@ s.connect((HOST, PORT))
 # The following several functions are to convert the given commands into a later seperatable string
 def move_piece(cellA,cellB):
     message = 'move_piece;' + str(cellA) + ';' + str(cellB)
-    print("Sent: %s" % (send(message)))
+    send(message)
+    #print("Sent: %s" % (send(message)))
 
 def move(cellA, cellB):
     message = 'move;' + str(cellA) + ';' + str(cellB)
-    print("Sent: %s" % (send(message)))
+    send(message)
+    #print("Sent: %s" % (send(message)))
 
 def take_piece(cellA, cellB, piece):
     message = 'take_piece;' + str(cellA) + ';' + str(cellB) + ';' + str(piece)
-    print("Sent: %s" % (send(message)))
+    send(message)
+    #print("Sent: %s" % (send(message)))
 
 def perform_castling_at(cellA, cellB, cellC, cellD):
     message = 'perform_castling_at;' + str(cellA) + ';' + str(cellB) + ';' + str(cellC) + ';' + str(cellD)
-    print("Sent: %s" % (send(message)))
+    send(message)
+    #print("Sent: %s" % (send(message)))
 
 def reset():
     message = 'reset;'
-    print("Sent: %s" % (send(message)))
+    send(message)
+    #print("Sent: %s" % (send(message)))
 
 # This function is to send the message to the server(ev3)
 def send(message):
+    print('Will send' + message)
     s.sendall(str.encode(message))
-    return s.recv(1024)
+    data = s.recv(1024)
+    print('Received', repr(data))
+    return data
 
 
 move_piece(('A',1),('A',2))
-#print('Received', repr(data))
 take_piece(('A',3),('A',4),'black_queen')
