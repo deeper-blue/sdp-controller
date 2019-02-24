6f2ed0f39a9c745f097a4c8234c49731cc13e012
CWJWANJING <wanjingchencwj@gmail.com>
2019-02-24 12:06:43 +0000

    rearranged the codes to make it neater

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+# The major functions of client are converting given command into a separatable string
+# and send the string to the server(ev3)
+# Author(s):
+#   Wanjing Chen
 
 import socket
 
@@ -7,29 +11,28 @@ PORT = 65432        # The port used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
+# The following several functions are to convert the given commands into a later seperatable string
 def move_piece(cellA,cellB):
-    message = 'move_piece;' + str(cellA) + ';' + str(cellB) + ';'
-    print("Received: %s" % (send(message)))
+    message = 'move_piece;' + str(cellA) + ';' + str(cellB)
 
 def move(cellA, cellB):
-    message = 'move;' + str(cellA) + ';' + str(cellB) + ';'
-    print("Received: %s" % (send(message)))
+    message = 'move;' + str(cellA) + ';' + str(cellB)
 
 def take_piece(cellA, cellB, piece):
-    message = 'take_piece;' + str(cellA) + ';' + str(cellB) + ';' + str(piece) + ';'
-    print("Received: %s" % (send(message)))
+    message = 'take_piece;' + str(cellA) + ';' + str(cellB) + ';' + str(piece)
 
 def perform_castling_at(cellA, cellB, cellC, cellD):
-    message = 'perform_castling_at;' + str(cellA) + ';' + str(cellB) + ';' + str(cellC) + ';' + str(cellD) + ';'
-    print("Received: %s" % (send(message)))
+    message = 'perform_castling_at;' + str(cellA) + ';' + str(cellB) + ';' + str(cellC) + ';' + str(cellD)
 
 def reset():
     message = 'reset;'
-    print("Received: %s" % (send(message)))
 
+# This function is to send the message to the server(ev3)
 def send(message):
     s.sendall(str.encode(message))
     return s.recv(1024)
+
+print("Sent: %s" % (send(message)))
 
 move_piece(('A',1),('A',2))
 #print('Received', repr(data))
