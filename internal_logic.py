707a111a47b262937da9c1c313390a502fde80db
Stewart Wilson <stewart.wilson98@gmail.com>
2019-03-08 19:30:43 +0000

    Added castling and fixed if's

--- internal_logic.py
+++ internal_logic.py
@@ -1,15 +1,16 @@
 import json
 import client as cl
 
+with open('test.json') as f:
+    data = json.load(f)
 
 
-def parseJson(jsonfile):
-    with open('test.json') as f:
-        data = json.load(f)
+def parseJson(data):
+
     move_count = data["move_count"]
     move_data = data["history"]["move_count"]
 
-     _from = move_data["from"]
+    _from = move_data["from"]
     to = move_data["to"]
 
     rowA, colA = _from[:1], int(_from[1:])
@@ -19,24 +20,25 @@ def parseJson(jsonfile):
     cellB = (rowB,colB)
 
     # Do take_piece
-    if(move_data["capture"]["capture"]) {
+    if(move_data["capture"]["capture"]):
         piece = move_data["capture"]["capture"]["piece"]
         cl.take_piece(cellA, cellB, piece)
-    }
     # Do castling
-    elif(move_data["castle"]["castle"]) {
-
+    elif(move_data["castle"]["castle"]):
+        if (move_data["castle"]["side"]=='k'):
+            cellC = (ord(cellA[0]+2),cellA[1])
+            cellD = (ord(cellB[0]-2),cellB[1])
+        else:
+            cellC = (ord(cellA[0]-2),cellA[1])
+            cellD = (ord(cellB[0]+3),cellB[1])
         cl.perform_castling_at(cellA, cellB, cellC, cellD)
-    }
     # Do en_passant
-    elif(move_data["en_passant"]["en_passant"]) {
+    elif(move_data["en_passant"]["en_passant"]):
         piece = ["piece"]
         cellTake = move_data["en_passant"]["en_passant"]["square"]
         cl.en_passant(cellA, cellB, cellTake, piece)
-    }
     # Do move_piece
-    else {
+    else:
         cl.move_piece(cellA, cellB)
-    }
 
-parseJson(jsonfile)
+parseJson(data)
